041dce9e69b3d6505579076adc1fcde5c628f57c
Daniel Todd <danielctodd10281990@gmail.com>
2022-04-29 22:29:06 -0600

    Add logic to determine winner
    Check how many times win, tie or loss
    is determined. Also make sure only valid
    values are accepted.

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function playRound(playerSelection, computerSelection) {
     ) {
       return "You win! Scissors cuts paper.";
     } else {
-      return "Tie! You both chose " + playerSelection + ". No one wins.";
+      return "Tie! You both chose " + playerSelection + ".";
     }
   } else {
     return 0;
@@ -48,6 +48,9 @@ function game() {
   console.log("First to 5 wins.");
   let choice;
   let result;
+  let win = 0;
+  let tie = 0;
+  let lose = 0;
   for (let i = 0; i < 5; i++) {
     choice = prompt("Enter your choice: ", "");
     result = playRound(choice, computerPlay());
@@ -55,8 +58,22 @@ function game() {
       console.log("Not a valid choice. Try again.");
       i -= 1;
     } else {
+      if (result.search(/win/i) != -1) {
+        win += 1;
+      }
+      if (result.search(/tie/i) != -1) {
+        tie += 1;
+      }
+      if (result.search(/lose/i) != -1) {
+        lose += 1;
+      }
       console.log(result);
     }
+  }
+  if (win >= 3 || (tie > lose && win > lose)) {
+    console.log(`You win! ${win} wins | ${lose} losses | ${tie} ties`);
+  } else {
+    console.log(`You lose! ${win} wins | ${lose} losses | ${tie} ties`);
   }
 }
 
