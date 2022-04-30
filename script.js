0b240d2ee24ad79f2641de9a5dd6666fc562db06
Daniel Todd <danielctodd10281990@gmail.com>
2022-04-29 21:35:22 -0600

    Create function to determine outcome of each game
    Check if user or computer chose correct. Return result.
    Also determine if there is a tie.

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function computerPlay() {
     return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper covers rock.";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock smashes scissors.";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper covers rock.";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose! Scissors cuts paper.";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose! Rock smashes scissors.";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors cuts paper.";
+  } else {
+    return "Tie! You both chose " + playerSelection + ". No one wins.";
+  }
+}
+
+console.log(playRound("rock", computerPlay()));
+console.log(playRound("paper", computerPlay()));
+console.log(playRound("scissors", computerPlay()));
