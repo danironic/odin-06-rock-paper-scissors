5466656fde1364f12229a1963c5bc999c895350d
Daniel Todd <danielctodd10281990@gmail.com>
2022-04-29 22:04:46 -0600

    Fix desc of game and function to play
    Change to best out of 5 and make function to play
    game a total of 5 times.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # odin-06-rock-paper-scissors
 
-### The sixth project for The Odin Project is to create a console version of rock, paper, scissors. The user plays against the computer until someone wins 5 times. The winner is announced.
+### The sixth project for The Odin Project is to create a console version of rock, paper, scissors. The user plays against the computer to see the best out of 5 matches. The winner is announced.
 
 GitHub Page: [Odin Rock, Paper, Scissors](https://danironic.github.io/odin-06-rock-paper-scissors/)
--- script.js
+++ script.js
@@ -12,23 +12,52 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose! Paper covers rock.";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock smashes scissors.";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper covers rock.";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose! Scissors cuts paper.";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose! Rock smashes scissors.";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors cuts paper.";
+  if (
+    playerSelection === "rock" ||
+    playerSelection === "paper" ||
+    playerSelection === "scissors"
+  ) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+      return "You lose! Paper covers rock.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      return "You win! Rock smashes scissors.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      return "You win! Paper covers rock.";
+    } else if (
+      playerSelection === "paper" &&
+      computerSelection === "scissors"
+    ) {
+      return "You lose! Scissors cuts paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      return "You lose! Rock smashes scissors.";
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      return "You win! Scissors cuts paper.";
+    } else {
+      return "Tie! You both chose " + playerSelection + ". No one wins.";
+    }
   } else {
-    return "Tie! You both chose " + playerSelection + ". No one wins.";
+    return 0;
   }
 }
 
-console.log(playRound("rock", computerPlay()));
-console.log(playRound("paper", computerPlay()));
-console.log(playRound("scissors", computerPlay()));
+function game() {
+  console.log("***ROCK___PAPER___SCISSORS***");
+  console.log("First to 5 wins.");
+  let choice;
+  let result;
+  for (let i = 0; i < 5; i++) {
+    choice = prompt("Enter your choice: ", "");
+    result = playRound(choice, computerPlay());
+    if (result === 0) {
+      console.log("Not a valid choice. Try again.");
+      i -= 1;
+    } else {
+      console.log(result);
+    }
+  }
+}
+
+game();
